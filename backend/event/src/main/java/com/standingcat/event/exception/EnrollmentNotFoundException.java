d0f67bb36bc6593fdb4042389a05897874a68948
Thalyonn <91253896+Thalyonn@users.noreply.github.com>
2025-08-07 17:21:19 +0800

    Added Enrollment Features

---
+++ backend/event/src/main/java/com/standingcat/event/exception/EnrollmentNotFoundException.java
@@ -0,0 +1,7 @@
+package com.standingcat.event.exception;
+
+public class EnrollmentNotFoundException extends RuntimeException{
+    public EnrollmentNotFoundException(String message) {
+        super(message);
+    }
+}
--- backend/event/src/main/java/com/standingcat/event/service/EnrollmentService.java
+++ backend/event/src/main/java/com/standingcat/event/service/EnrollmentService.java
@@ -1,5 +1,6 @@
 package com.standingcat.event.service;
 
+import com.standingcat.event.exception.EnrollmentNotFoundException;
 import com.standingcat.event.exception.EventNotFoundException;
 import com.standingcat.event.exception.UserNotFoundException;
 import com.standingcat.event.model.Enrollment;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class EnrollmentService {
@@ -45,6 +47,29 @@ public class EnrollmentService {
         return savedEnrollment;
 
 
+    }
+
+    @Transactional
+    public void unEnrollUserToEvent(Long userId, Long eventId) {
+        User user = userService.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("User not Found"));
+        Event event = eventService.getEventById(eventId)
+                .orElseThrow(() -> new EventNotFoundException("Event not Found"));
+        Enrollment enrollment = enrollmentRepository.findByUserAndEvent(user, event)
+                .orElseThrow(() -> new EnrollmentNotFoundException("Enrollment not Found"));
+        enrollmentRepository.delete(enrollment);
+    }
+
+    public List<Enrollment> getEnrollmentsForEvent(Long eventId) { 
+        Event event = eventService.getEventById(eventId)
+                .orElseThrow(() -> new RuntimeException("Event not found."));
+        return enrollmentRepository.findByEvent(event);
+    }
+
+    public List<Enrollment> getEnrollmentsForUser(Long userId) {
+        User user = userService.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found."));
+        return enrollmentRepository.findByUser(user);
     }
 
 
